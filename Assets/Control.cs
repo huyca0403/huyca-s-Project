e6c079cf3f46f9beaed88203512e8ad74908ba82
huyca <huycachuoi@gmail.com>
2024-07-29 14:26:52 +0700

    huy commit 1

---
+++ Assets/Control.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class Control : MonoBehaviour
+{
+    public int huong;
+    public int toc_do=10;
+    public GameObject main;
+    public bool nhay;
+    public int chieu_cao;
+    public Rigidbody2D rb;
+
+    // duoc goi o frame dau tien
+    void Start()
+    {
+        
+    }
+    // duoc goi o moi frame
+    void Update()
+    {
+        DiChuyenNhanVat();
+    }
+    void DiChuyenNhanVat()
+    {
+        //huong cua nhan di chuyen
+        if (Input.GetKey(KeyCode.LeftArrow)) this.huong = -1;
+        else if (Input.GetKey(KeyCode.RightArrow)) this.huong = 1;
+        else this.huong = 0;
+        //di chuyen nhan vat
+        main.transform.Translate(Vector3.right*toc_do*huong*Time.deltaTime);
+        //nhay
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && nhay == true)
+        {
+            rb.AddForce(Vector2.up * chieu_cao, ForceMode2D.Impulse);
+            nhay= false;
+        }
+    }
+}
